043715b4d6e848789f13c578bb0c40e9bf810a0f
Ahmed Samy <115529140+AhmedSamy9999@users.noreply.github.com>
2023-02-14 05:20:09 +0200

    create home dashboard

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { useList } from "@pankod/refine-core";
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent,
+} from "components";
+import { Box, Typography, Stack } from "@pankod/refine-mui";
 
 const home = () => {
-  return <div>home</div>;
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Dashboard
+      </Typography>
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart
+          title="Properties for Sale"
+          value={684}
+          series={[75, 25]}
+          colors={["#475BE8", "#a3adf4"]}
+        />
+        <PieChart
+          title="Properties for Rent"
+          value={550}
+          series={[60, 40]}
+          colors={["#FD8539", "#fec29c"]}
+        />
+        <PieChart
+          title="Total Customers"
+          value={5684}
+          series={[75, 25]}
+          colors={["#2ED480", "#97eac0"]}
+        />
+        <PieChart
+          title="Properties for Cities"
+          value={555}
+          series={[75, 25]}
+          colors={["#FE6D8E", "#fea7bb"]}
+        />
+      </Box>
+      <Stack
+        mt="25px"
+        width="100%"
+        direction={{ xs: "column", lg: "row" }}
+        gap={4}
+      >
+        <TotalRevenue />
+        <PropertyReferrals />
+      </Stack>
+    </Box>
+  );
 };
 
 export default home;
